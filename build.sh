03d1ea41880114acc33772745f080f18f1192ae9
tsdk <st_zzh@sina.com>
2019-04-27 23:36:15 +0800

    add script

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo ""
+
+echo -e "\nbuild docker image\n"
+docker build -t tsdk/dnsmasq-docker .
+
+echo ""
+
+
---
+++ run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+docker run \
+    --name dnsmasq \
+    -d \
+    -p 53:53/udp \
+    -p 5380:8080 \
+    -v ~/var/etc/dnsmasq.conf:/etc/dnsmasq.conf \
+    -e "HTTP_USER=root" \
+    -e "HTTP_PASS=123456" \
+    --restart always \
+    tsdk/dnsmasq-docker
+
+docker=dnsmasq
+ip=192.168.82.254
+gw=192.168.82.2
+sudo env "PATH=$PATH" pipework br0 $docker $ip/24@$gw
+docker exec -it $docker sh 
